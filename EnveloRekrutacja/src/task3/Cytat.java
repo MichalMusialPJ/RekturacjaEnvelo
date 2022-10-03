dd0c88c87f838b3a4e953e2b55aba73d1cfe5a95
MichalMusialPJ <93324928+MichalMusialPJ@users.noreply.github.com>
2022-10-03 15:28:50 +0200

    1st commit

---
+++ EnveloRekrutacja/src/task3/Cytat.java
@@ -0,0 +1,11 @@
+package task3;
+
+public class Cytat {
+    private String quote;
+    public String getQuote(){
+        return quote;
+    }
+    public void setQuote(String quote){
+        quote = this.quote;
+    }
+}
